df488d227e42ac65be537818e9ea9a26d2927eb8
SyedHamzaAzhar <85331751+SyedHamzaAzhar@users.noreply.github.com>
2022-08-27 11:29:35 +0500

    first commit

---
+++ src/users/roles.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Role } from "./entities/role.enum";
+import { User } from "./entities/user.entity";
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean {
+    const requireRoles = this.reflector.getAllAndOverride<Role[]>("roles", [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requireRoles) {
+      return true;
+    }
+    const user: User = {
+      name: "Hamza",
+      roles: [Role.ADMIN, Role.USER],
+    };
+    return requireRoles.some((role) => user.roles.includes(role));
+  }
+}
